dc58ea432bcd58d6bcce90636cf6f843538794e0
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-09-02 14:47:23 -0300

    finalizando backend e criando nextJS

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -37,6 +37,64 @@ const partyController = {
     } catch (error) {
       console.log(error)
     }
+  },
+
+  get: async (req, res) => {
+    try {
+      const party = await PartyModel.findById(req.params.id)
+      if (!party) {
+        res.status(404).json({ msg: "Festa não encontrada." })
+        return;
+      }
+      res.json(party)
+
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const party = await PartyModel.findById(req.params.id)
+      if (!party) {
+        res.status(404).json({ msg: "Festa não encontrada." })
+        return;
+      }
+      const deleteParty = await PartyModel.findByIdAndDelete(req.params.id)
+      res.status(200).json({ deleteParty, msg: "A festa foi removida." })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      }
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu saldo é insuficiente." })
+        return;
+      }
+
+      const updatedParty = await PartyModel.findByIdAndUpdate(req.params.id, party)
+
+      if (!updatedParty) {
+        res.status(404).json({ msg: "Festa não encontrada." })
+        return;
+      }
+
+      res.status(200).json({ party, msg: "Festa atualizada com sucesso!" })
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
